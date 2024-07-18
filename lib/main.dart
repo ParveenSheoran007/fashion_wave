c454f05376fcf2da063f0c823a9fd4370a94702a
Parveen <parveensheoran7079@gmail.com>
2024-07-18 11:12:48 +0530

    ui change basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:fashion_wave/auth/ui/login_screen.dart';
+import 'package:fashion_wave/product/ui/product_screen.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Fashion Wave',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LogInScreen(),
+        '/product': (context) => const ProductScreen(),
+      },
+    );
+  }
+}
